31c1d2070c502a2c876472866facc9d3126af820
Cesar A. Luis <cesarluis13915@gmail.com>
2019-06-14 18:31:15 -0600

    Getting started with route params, query params, fragment and nested routes.

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+  paramsSubscription: Subscription;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.user = {
+      id: this.route.snapshot.params['id'],
+      name: this.route.snapshot.params['name']
+    };
+
+    // NOTE: Use .subscribe() when the route can be reached within itself with
+    // diferent parameters.
+    this.paramsSubscription = this.route.params
+      .subscribe((params: Params) => {
+        this.user.id = params['id'];
+        this.user.name  = params['name'];
+      });
   }
 
+  ngOnDestroy() {
+    // NOTE: Is not neccessary because Angular already do it for us, but in order
+    // to understand what is happending behind the scenes, can be added manually
+    // and it doesn't hurt our application.
+    this.paramsSubscription.unsubscribe();
+  }
 }
